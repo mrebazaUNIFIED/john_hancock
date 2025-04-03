1578877c4e1428a0119d6cf86479219acaf1bfdb
MarcoAguilar2002 <marco.ra2002@gmail.com>
2025-04-03 09:54:38 -0500

    New

---
+++ backend/requerimientos.bat
@@ -0,0 +1,13 @@
+pip install asgiref
+pip install Django
+pip install django-cors-headers
+pip install djangorestframework 
+pip install djangorestframework-simplejwt
+pip install PyJWT
+pip install pytz
+pip install sqlparse
+pip installpsycopg2-binary
+pip installpython-dotenv
+pip install wfastcgi
+
+
